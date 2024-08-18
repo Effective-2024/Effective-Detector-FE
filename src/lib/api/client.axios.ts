1db929cf854899749e6e3ba81f6e89b3fa7db2d6
amaran-th <songsy405@naver.com>
2024-08-18 16:32:27 +0900

    build: env 파일 설정 변경

--- src/lib/api/client.axios.ts
+++ src/lib/api/client.axios.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { categorizeError } from '~/types/error';
 
 export const client = axios.create({
-  baseURL: process.env.SERVER_URL,
+  baseURL: process.env.REACT_APP_SERVER_URL,
   withCredentials: true,
 });
 
