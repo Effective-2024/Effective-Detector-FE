0ec302752b306e1145d085bfa9fba99718b369b7
amaran-th <songsy405@naver.com>
2024-09-09 21:01:43 +0900

    design: 로그인 페이지 디자인 리뉴얼

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,6 +1,5 @@
-import { ReactComponent as Logo } from '@assets/logo/logo.svg';
 import { useAppDispatch } from '@lib/hooks/redux';
-import { Box, Button, TextField } from '@mui/material';
+import { Box, Button, Divider, TextField } from '@mui/material';
 import { login } from '@store/member.slice';
 import { Form, Formik, FormikHelpers } from 'formik';
 import { Link, useNavigate } from 'react-router-dom';
@@ -50,57 +49,54 @@ const Login = () => {
   };
 
   return (
-    <div className="mx-auto w-full max-w-sign">
-      <div className="my-[88px] flex flex-col gap-6">
-        <Logo />
-        <Box className="py-4">
-          <div>
-            <p className="text-xl">로그인</p>
-          </div>
-          <div className="mt-2 border-b border-comment" />
-        </Box>
-        <div className="flex flex-col gap-8">
-          <Formik initialValues={initialValues} onSubmit={handleSubmit}>
-            {({ isSubmitting, values, handleChange, handleBlur }) => (
-              <Form className="flex flex-col gap-4">
-                <TextField
-                  type="text"
-                  name="id"
-                  label="ID"
-                  value={values.id}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                />
-                <TextField
-                  type="password"
-                  name="password"
-                  label="Password"
-                  value={values.password}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                />
+    <div className="flex h-full items-center justify-center">
+      <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+        {({ isSubmitting, values, handleChange, handleBlur }) => (
+          <Form className="flex w-full max-w-sign flex-col gap-2 rounded-lg bg-white px-10 py-16 shadow-md">
+            <div className="flex flex-col gap-2">
+              <Box className="py-4">
+                <p className="mb-1 text-xl">로그인</p>
+                <Divider />
+              </Box>
 
-                <div className="flex flex-col">
-                  <Button
-                    type="submit"
-                    variant="contained"
-                    size="large"
-                    disabled={isSubmitting}
-                  >
-                    로그인
-                  </Button>
-                  <div className="mt-3 flex justify-center gap-6 text-comment">
-                    Safe Catcher에 등록되어 있지 않나요?
-                    <Link to="/sign-up" className="font-bold underline">
-                      회원가입하기
-                    </Link>
-                  </div>
-                </div>
-              </Form>
-            )}
-          </Formik>
-        </div>
-      </div>
+              <TextField
+                type="text"
+                name="id"
+                label="ID"
+                value={values.id}
+                onChange={handleChange}
+                onBlur={handleBlur}
+              />
+              <TextField
+                type="password"
+                name="password"
+                label="Password"
+                value={values.password}
+                onChange={handleChange}
+                onBlur={handleBlur}
+              />
+
+              <Button
+                type="submit"
+                variant="contained"
+                size="large"
+                disabled={isSubmitting}
+                sx={{
+                  marginTop: '16px',
+                }}
+              >
+                로그인
+              </Button>
+            </div>
+            <div className="flex justify-center gap-6 text-primary">
+              Safe Catcher에 등록되어 있지 않나요?
+              <Link to="/sign-up" className="font-bold underline">
+                회원가입하기
+              </Link>
+            </div>
+          </Form>
+        )}
+      </Formik>
     </div>
   );
 };
