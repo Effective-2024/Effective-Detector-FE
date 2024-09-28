9a5d8fe7d34756bc69df96e659b974ea23cd1d75
amaran-th <songsy405@naver.com>
2024-09-28 16:13:05 +0900

    feat: toastify 라이브러리 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import ReduxProvider from '@lib/utils/reduxProvider';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import useMainRouter from '@router/useMainRouter';
+import { ToastContainer } from 'react-toastify';
 import ReactQueryProvider from './lib/utils/queryProvider';
+
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   const router = useMainRouter();
@@ -20,6 +23,7 @@ function App() {
       <ReactQueryProvider>
         <ReduxProvider>
           <div className="font-pretendard">{router}</div>
+          <ToastContainer autoClose={1500} position="bottom-right" />
         </ReduxProvider>
       </ReactQueryProvider>
     </ThemeProvider>
